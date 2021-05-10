d568730e6963ccbfe8119adad5405eff2858c477
shapedgod <hi@parthiv.dev>
2021-05-10 16:49:35 +0000

    format

--- demo/angular-restaurant-menu/js/menu.js
+++ demo/angular-restaurant-menu/js/menu.js
@@ -1,81 +1,68 @@
-var Menu = (function() {
-	
-	var $container = $( '#rm-container' ),						
-		$cover = $container.find( 'div.rm-cover' ),
-		$middle = $container.find( 'div.rm-middle' ),
-		$right = $container.find( 'div.rm-right' ),
-		$open = $cover.find('a.rm-button-open'),
-		$close = $right.find('span.rm-close'),
-		$details = $container.find( 'a.rm-viewdetails' ),
+var Menu = (function () {
+  var $container = $("#rm-container"),
+    $cover = $container.find("div.rm-cover"),
+    $middle = $container.find("div.rm-middle"),
+    $right = $container.find("div.rm-right"),
+    $open = $cover.find("a.rm-button-open"),
+    $close = $right.find("span.rm-close"),
+    $details = $container.find("a.rm-viewdetails"),
+    init = function () {
+      initEvents();
+    },
+    initEvents = function () {
+      $open.on("click", function (event) {
+        openMenu();
+        return false;
+      });
 
-		init = function() {
+      $close.on("click", function (event) {
+        closeMenu();
+        return false;
+      });
 
-			initEvents();
+      $details.on("click", function (event) {
+        $container.removeClass("rm-in").children("div.rm-modal").remove();
+        viewDetails($(this));
+        return false;
+      });
+    },
+    openMenu = function () {
+      $container.addClass("rm-open");
+    },
+    closeMenu = function () {
+      $container.removeClass("rm-open rm-nodelay rm-in");
+    },
+    viewDetails = function (recipe) {
+      var title = recipe.text(),
+        img = recipe.data("thumb"),
+        description = recipe.parent().next().text(),
+        url = recipe.attr("href");
 
-		},
-		initEvents = function() {
+      var $modal = $(
+        '<div class="rm-modal"><div class="rm-thumb" style="background-image: url(' +
+          img +
+          ')"></div><h5>' +
+          title +
+          "</h5><p>" +
+          description +
+          '</p><a href="' +
+          url +
+          '">See the recipe</a><span class="rm-close-modal">x</span></div>'
+      );
 
-			$open.on( 'click', function( event ) {
+      $modal.appendTo($container);
 
-				openMenu();
-				return false;
+      var h = $modal.outerHeight(true);
+      $modal.css("margin-top", -h / 2);
 
-			} );
+      setTimeout(function () {
+        $container.addClass("rm-in rm-nodelay");
 
-			$close.on( 'click', function( event ) {
+        $modal.find("span.rm-close-modal").on("click", function () {
+          $container.removeClass("rm-in");
+        });
+      }, 0);
+    };
 
-				closeMenu();
-				return false;
-
-			} );
-
-			$details.on( 'click', function( event ) {
-
-				$container.removeClass( 'rm-in' ).children( 'div.rm-modal' ).remove();
-				viewDetails( $( this ) );
-				return false;
-
-			} );
-			
-		},
-		openMenu = function() {
-
-			$container.addClass( 'rm-open' );
-
-		},
-		closeMenu = function() {
-
-			$container.removeClass( 'rm-open rm-nodelay rm-in' );
-
-		},
-		viewDetails = function( recipe ) {
-
-			var title = recipe.text(),
-				img = recipe.data( 'thumb' ),
-				description = recipe.parent().next().text(),
-				url = recipe.attr( 'href' );
-
-			var $modal = $( '<div class="rm-modal"><div class="rm-thumb" style="background-image: url(' + img + ')"></div><h5>' + title + '</h5><p>' + description + '</p><a href="' + url + '">See the recipe</a><span class="rm-close-modal">x</span></div>' );
-
-			$modal.appendTo( $container );
-
-			var h = $modal.outerHeight( true );
-			$modal.css( 'margin-top', -h / 2 );
-
-			setTimeout( function() {
-
-				$container.addClass( 'rm-in rm-nodelay' );
-
-				$modal.find( 'span.rm-close-modal' ).on( 'click', function() {
-
-					$container.removeClass( 'rm-in' );
-
-				} );
-			
-			}, 0 );
-
-		};
-
-	return { init : init };
-
+  return { init: init };
 })();
